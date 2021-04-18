932afc00b753bd202897e53d1d805b220c88d9e4
rudi9999 <67137156+rudi9999@users.noreply.github.com>
2021-04-17 21:48:12 -0300

    Add files via upload

--- node/install.sh
+++ node/install.sh
@@ -2,4 +2,8 @@
 
 botdir="botgrupo" && [[ ! -d botgrupo ]] && mkdir botgrupo
 
-read foo 
+wget -O botgrupo/bot.sh https://raw.githubusercontent.com/rudi9999/simple_bot/main/node/bot.js
+wget -O botgrupo/package.json https://raw.githubusercontent.com/rudi9999/simple_bot/main/node/package.json
+wget -O botgrupo/confbot https://raw.githubusercontent.com/rudi9999/simple_bot/main/node/confbot
+cd botgrupo
+npm update --save
